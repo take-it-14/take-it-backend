7e640e597e3bd6ba979a9f54b5bb9adebe2ed773
ykchoi1203 <chldudrms94@naver.com>
2025-01-02 16:13:32 +0900

    [Feat]: Review Entity 생성

---
+++ review-service/src/main/java/com/takeit/review/domain/entity/Review.java
@@ -0,0 +1,36 @@
+package com.takeit.review.domain.entity;
+
+import com.takeit.common.domain.model.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table(name = "p_review")
+@Builder
+public class Review extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "uuid",unique = true, nullable = false)
+    private UUID uuid;
+
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "stars", nullable = false)
+    private Integer stars;
+
+    @Column(name = "comment")
+    private String comment;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "id")
+    private List<ReviewPhoto> photoList;
+
+}
