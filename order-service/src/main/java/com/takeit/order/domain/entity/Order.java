90544808421ab38b95725a233ae6d7299218313f
MinJeong LEE <alswjd6807@naver.com>
2025-01-02 11:42:16 +0900

    [Feat] : Order entity, OrderStatus 생성

---
+++ order-service/src/main/java/com/takeit/order/domain/entity/Order.java
@@ -0,0 +1,52 @@
+package com.takeit.order.domain.entity;
+
+import java.util.UUID;
+
+import com.takeit.common.domain.model.BaseEntity;
+import com.takeit.order.domain.enums.OrderStatus;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "p_order")
+public class Order extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(nullable = false)
+	private UUID uuid;
+
+	@Column(nullable = false)
+	private Long customerId;
+
+	@Column(nullable = false)
+	private Long productId;
+
+	@Column(nullable = false)
+	private Long quantity;
+
+	@Column(nullable = false)
+	private Long amount;
+
+	@Column(nullable = false)
+	@Enumerated(EnumType.STRING)
+	private OrderStatus status;
+
+}
