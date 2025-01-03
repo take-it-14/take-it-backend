10e132e278c411d10f32c55f670fb9ccac359dc4
rua0704 <rua0704@gmail.com>
2025-01-03 14:10:48 +0900

    feat - Favorite Repository 구현

--- favorite-service/src/main/java/com/takeit/favorite/domain/repository/FavoriteRepository.java
+++ favorite-service/src/main/java/com/takeit/favorite/domain/repository/FavoriteRepository.java
@@ -1,4 +1,7 @@
 package com.takeit.favorite.domain.repository;
 
+import com.takeit.favorite.domain.entity.Favorite;
+
 public interface FavoriteRepository {
+    Favorite save(Favorite favorite);
 }
