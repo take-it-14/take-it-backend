a4fd37baffc25e23ea5b7c064068012d5c90aa12
MinJeong LEE <alswjd6807@naver.com>
2025-01-04 01:12:18 +0900

    [Refactor] OrderCreateResponse->OrderResponse 이름 변경

--- order-service/src/main/java/com/takeit/order/application/dto/OrderResponse.java
+++ order-service/src/main/java/com/takeit/order/application/dto/OrderResponse.java
@@ -4,14 +4,14 @@ import java.util.UUID;
 
 import com.takeit.order.domain.entity.Order;
 
-public record OrderCreateResponse(
+public record OrderResponse(
 	UUID orderId,
 	UUID productId,
 	Long quantity,
 	Long amount
 ) {
-	public static OrderCreateResponse from(Order order, UUID productId) {
-		return new OrderCreateResponse(
+	public static OrderResponse from(Order order, UUID productId) {
+		return new OrderResponse(
 			order.getUuid(),
 			productId,
 			order.getQuantity(),
