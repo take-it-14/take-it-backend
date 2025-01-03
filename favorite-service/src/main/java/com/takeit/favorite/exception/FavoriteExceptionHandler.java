4c2b495d571f8a5637ee8fd174ca3248868cf51e
rua0704 <rua0704@gmail.com>
2025-01-03 12:09:32 +0900

    fix - Exception Handler 이름 수정

--- favorite-service/src/main/java/com/takeit/favorite/exception/FavoriteExceptionHandler.java
+++ favorite-service/src/main/java/com/takeit/favorite/exception/FavoriteExceptionHandler.java
@@ -1,8 +1,8 @@
 package com.takeit.favorite.exception;
 
-import com.takeit.common.exception.CommonExceptionHandler;
+import com.takeit.common.exception.GlobalExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class DeliveryExceptionHandler extends CommonExceptionHandler {
+public class FavoriteExceptionHandler extends GlobalExceptionHandler {
 }
