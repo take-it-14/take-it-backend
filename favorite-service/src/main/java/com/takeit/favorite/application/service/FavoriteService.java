6d18f0cd0754c67b47a4035d612492c63518bec9
rua0704 <rua0704@gmail.com>
2025-01-03 14:11:20 +0900

    feat - service 구현

--- favorite-service/src/main/java/com/takeit/favorite/application/service/FavoriteService.java
+++ favorite-service/src/main/java/com/takeit/favorite/application/service/FavoriteService.java
@@ -1,5 +1,9 @@
 package com.takeit.favorite.application.service;
 
+import com.takeit.favorite.application.dto.favorite.CreateFavoriteDto;
+import com.takeit.favorite.application.dto.favorite.CreateFavoriteResponse;
+import com.takeit.favorite.domain.entity.Favorite;
+import com.takeit.favorite.domain.repository.FavoriteRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -8,4 +12,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class FavoriteService {
+    private final FavoriteRepository favoriteRepository;
+
+    @Transactional
+    public CreateFavoriteResponse createFavorite(CreateFavoriteDto request, String username) {
+        // TODO: username으로 User 정보 받아오는 메소드 추가 + 권한 체크
+        Long userId = 1L;
+        // TODO: UUID로 Product 정보 받아오는 메소드 추가
+        Long productId = 1L;
+
+        Favorite favorite = favoriteRepository.save(
+                Favorite.create(productId, userId)
+        );
+
+        return CreateFavoriteResponse.of(favorite.getUuid(), username,  request.getProductId());
+    }
 }
