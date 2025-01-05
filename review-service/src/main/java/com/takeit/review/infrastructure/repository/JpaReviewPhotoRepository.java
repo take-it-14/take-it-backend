a5c47b66b35fef27295bb3a639830bc527e743d2
ykchoi1203 <chldudrms94@naver.com>
2025-01-06 06:47:49 +0900

    [Feat]: update 관련 repository method 추가

--- review-service/src/main/java/com/takeit/review/infrastructure/repository/JpaReviewPhotoRepository.java
+++ review-service/src/main/java/com/takeit/review/infrastructure/repository/JpaReviewPhotoRepository.java
@@ -1,7 +1,20 @@
 package com.takeit.review.infrastructure.repository;
 
+import com.takeit.review.domain.entity.Review;
 import com.takeit.review.domain.entity.ReviewPhoto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.UUID;
 
 public interface JpaReviewPhotoRepository extends JpaRepository<ReviewPhoto, Long> {
+    @Modifying
+    @Query("update ReviewPhoto set isDeleted = true, deletedAt = CURRENT_TIMESTAMP, deletedBy = :username, updatedAt = CURRENT_TIMESTAMP, updatedBy = :username where uuid in :uuids")
+    int deleteAllByUuidIn(List<UUID> uuids, String username);
+
+    List<ReviewPhoto> findByReviewAndIsDeletedIsFalse(Review review);
+
+    List<ReviewPhoto> findAllByReviewAndIsDeletedIsFalse(Review review);
 }
--- review-service/src/main/java/com/takeit/review/infrastructure/repository/JpaReviewRepository.java
+++ review-service/src/main/java/com/takeit/review/infrastructure/repository/JpaReviewRepository.java
@@ -2,6 +2,12 @@ package com.takeit.review.infrastructure.repository;
 
 import com.takeit.review.domain.entity.Review;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+import java.util.UUID;
 
 public interface JpaReviewRepository extends JpaRepository<Review, Long> {
+    @Query("select r from Review r where r.uuid = :reviewId and r.isDeleted = false")
+    Optional<Review> findByUuidAndIsDeletedIsFalse(UUID reviewId);
 }
