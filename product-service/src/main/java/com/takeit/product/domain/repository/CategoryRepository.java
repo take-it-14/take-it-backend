398bb74b7f5a1429decc6d4de8690b7b1cdcb141
rua0704 <rua0704@gmail.com>
2025-01-07 20:00:34 +0900

    feat - 카테고리 생성 repository 구현

--- product-service/src/main/java/com/takeit/product/domain/repository/CategoryRepository.java
+++ product-service/src/main/java/com/takeit/product/domain/repository/CategoryRepository.java
@@ -1,4 +1,7 @@
 package com.takeit.product.domain.repository;
 
+import com.takeit.product.domain.entity.Category;
+
 public interface CategoryRepository {
+    Category createCategory(Category category);
 }
--- product-service/src/main/java/com/takeit/product/infrastructure/repository/category/CategoryRepositoryImpl.java
+++ product-service/src/main/java/com/takeit/product/infrastructure/repository/category/CategoryRepositoryImpl.java
@@ -1,8 +1,17 @@
 package com.takeit.product.infrastructure.repository.category;
 
+import com.takeit.product.domain.entity.Category;
 import com.takeit.product.domain.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class CategoryRepositoryImpl implements CategoryRepository {
+    private final CategoryJpaRepository jpaRepository;
+
+    @Override
+    public Category createCategory(Category category) {
+        return jpaRepository.save(category);
+    }
 }
