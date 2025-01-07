6a670d6feedf6072d1908f8ba6c974bb3cd763c0
rua0704 <rua0704@gmail.com>
2025-01-07 20:00:55 +0900

    feat - 카테고리 생성 controller 구현 & 테스트 완료

--- product-service/src/main/java/com/takeit/product/presentation/controller/CategoryController.java
+++ product-service/src/main/java/com/takeit/product/presentation/controller/CategoryController.java
@@ -1,13 +1,22 @@
 package com.takeit.product.presentation.controller;
 
+import com.takeit.common.presentation.dto.CommonResponse;
+import com.takeit.product.application.dto.category.CategoryResponse;
 import com.takeit.product.application.service.CategoryService;
+import com.takeit.product.presentation.request.CreateCategoryRequest;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/reviews")
+@RequestMapping("/api/v1/categories")
 public class CategoryController {
     private final CategoryService categoryService;
+
+    @PostMapping
+    public CommonResponse<CategoryResponse> createCategory(@Valid @RequestBody CreateCategoryRequest request,
+                                                           @RequestHeader(value = "X-Username") String username) {
+        return CommonResponse.ofSuccess("카테고리 생성", categoryService.createCategory(request.toDto(), username));
+    }
 }
