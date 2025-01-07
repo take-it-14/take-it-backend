4fa71acf686e7f959432ba1d90e03b0fe0ab7151
ykchoi1203 <chldudrms94@naver.com>
2025-01-07 15:44:39 +0900

    [Feat]: Auditing 추가

---
+++ coupon-service/src/main/java/com/takeit/coupon/infrastructure/config/AuditorAwareImpl.java
@@ -0,0 +1,28 @@
+package com.takeit.coupon.infrastructure.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes instanceof ServletRequestAttributes) {
+            HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+            String username = request.getHeader("X-Username");
+            if (username != null) {
+                return Optional.of(username);
+            }
+        }
+
+        return Optional.of("system");
+    }
+}
