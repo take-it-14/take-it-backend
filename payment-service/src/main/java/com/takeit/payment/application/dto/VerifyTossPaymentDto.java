c0c88adda505998b396e0d107929aa6522cea3bc
rua0704 <rua0704@gmail.com>
2025-01-07 13:54:27 +0900

    feat - 결제 검증은 외부 서비스이기 때문에 UUID로 변경

--- payment-service/src/main/java/com/takeit/payment/application/dto/VerifyTossPaymentDto.java
+++ payment-service/src/main/java/com/takeit/payment/application/dto/VerifyTossPaymentDto.java
@@ -1,12 +1,14 @@
 package com.takeit.payment.application.dto;
 
+import java.util.UUID;
+
 public record VerifyTossPaymentDto(
-        Long orderId,
+        UUID orderId,
         String tossPaymentOrderId,
         String paymentKey,
         Integer amount
 ) {
-    public static VerifyTossPaymentDto of(Long orderId, String tossPaymentOrderId, String paymentKey, Integer amount) {
+    public static VerifyTossPaymentDto of(UUID orderId, String tossPaymentOrderId, String paymentKey, Integer amount) {
         return new VerifyTossPaymentDto(orderId, tossPaymentOrderId, paymentKey, amount);
     }
 }
