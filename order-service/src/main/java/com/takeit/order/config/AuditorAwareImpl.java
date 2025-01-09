7eae2508e224c0049b305e37bfde74010dee268c
MinJeong LEE <alswjd6807@naver.com>
2025-01-09 21:11:20 +0900

    [Feat] : JpaAudit 적용

---
+++ order-service/src/main/java/com/takeit/order/config/AuditorAwareImpl.java
@@ -0,0 +1,29 @@
+package com.takeit.order.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import java.util.Optional;
+
+@Component
+public class AuditorAwareImpl implements AuditorAware<String> {
+	@Override
+	public Optional<String> getCurrentAuditor() {
+		RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+		if (requestAttributes instanceof ServletRequestAttributes) {
+			HttpServletRequest request = ((ServletRequestAttributes) requestAttributes).getRequest();
+
+			String username = request.getHeader("X-Username");
+			if (username != null) {
+				return Optional.of(username);
+			}
+		}
+
+
+		return Optional.of("system");
+	}
+}
