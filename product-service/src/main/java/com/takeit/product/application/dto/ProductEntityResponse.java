82e76c9d25696dfa276316469a9f64315b4acaa2
rua0704 <rua0704@gmail.com>
2025-01-09 11:17:17 +0900

    feat - product UUID 로 Product 조회 기능 구현

---
+++ product-service/src/main/java/com/takeit/product/application/dto/ProductEntityResponse.java
@@ -0,0 +1,33 @@
+package com.takeit.product.application.dto;
+
+import com.takeit.product.domain.entity.Product;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ProductEntityResponse(
+        Long id,
+        UUID ProductId,
+        String productName,
+        String description,
+        Long price,
+        Integer stock,
+        Integer limitPerUser,
+        LocalDateTime openTime,
+        LocalDateTime closeTime,
+        Boolean isActive
+) {
+    public static ProductEntityResponse from(Product product) {
+        return new ProductEntityResponse(
+                product.getId(),
+                product.getUuid(),
+                product.getProductName(),
+                product.getDescription(),
+                product.getPrice(),
+                product.getStock(),
+                product.getLimitPerUser(),
+                product.getOpenTime(),
+                product.getCloseTime(),
+                product.getIsActive());
+    }
+}
--- product-service/src/main/java/com/takeit/product/presentation/controller/ProductEndPoint.java
+++ product-service/src/main/java/com/takeit/product/presentation/controller/ProductEndPoint.java
@@ -1,9 +1,14 @@
 package com.takeit.product.presentation.controller;
 
+import com.takeit.product.application.dto.ProductEntityResponse;
 import com.takeit.product.application.service.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -11,4 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductEndPoint {
     private final ProductService productService;
 
+    @GetMapping("/uuid/{productId}")
+    public ProductEntityResponse getProductByUuid(@PathVariable UUID productId) {
+        return productService.getProductEntity(productId);
+    }
 }
