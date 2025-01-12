28e544b6b690f25086068d215107fba007d88923
MinJeong LEE <60657536+M1ngD0ng@users.noreply.github.com>
2025-01-12 19:41:14 +0900

    [Feat] : OrderExceptionHandler 추가

---
+++ order-service/src/main/java/com/takeit/order/exception/OrderExceptionHandler.java
@@ -0,0 +1,9 @@
+package com.takeit.order.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.takeit.common.exception.GlobalExceptionHandler;
+
+@RestControllerAdvice
+public class OrderExceptionHandler extends GlobalExceptionHandler {
+}
