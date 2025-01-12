4a6f7db95fe2c2c240eea430583eeafb3d88b208
rua0704 <rua0704@gmail.com>
2025-01-12 17:08:22 +0900

    fix - ProductDetailResponse 오타 수정

--- product-service/src/main/java/com/takeit/product/application/dto/product/ProductDetailResponse.java
+++ product-service/src/main/java/com/takeit/product/application/dto/product/ProductDetailResponse.java
@@ -6,7 +6,7 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record ProductDetailResponse(
-        UUID ProductId,
+        UUID productId,
         String productName,
         String description,
         Long price,
