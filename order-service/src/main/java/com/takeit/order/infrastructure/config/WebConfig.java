5b1a45801fea40e65e85562415a3413080e83a48
MinJeong LEE <alswjd6807@naver.com>
2025-01-14 16:19:48 +0900

    [Refactor] : config 패키지 이동

--- order-service/src/main/java/com/takeit/order/infrastructure/config/WebConfig.java
+++ order-service/src/main/java/com/takeit/order/infrastructure/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.takeit.order.config;
+package com.takeit.order.infrastructure.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
