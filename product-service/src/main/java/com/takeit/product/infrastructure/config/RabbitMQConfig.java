450852d8598ab30a4ab83e65eb92cb87be976ed2
ykchoi1203 <chldudrms94@naver.com>
2025-01-14 12:24:52 +0900

    [Feat]: product rabbitmq config, listener 추가 및 결제 취소 시 보상 트랜잭션 추가

---
+++ product-service/src/main/java/com/takeit/product/infrastructure/config/RabbitMQConfig.java
@@ -0,0 +1,32 @@
+package com.takeit.product.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+
+    @Value("${message.queue.product.cancel}")
+    private String queueProduct;
+
+    @Bean
+    public Queue productQueue() { return new Queue(queueProduct); }
+
+//    @Bean
+//    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+//        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+//        factory.setConnectionFactory(connectionFactory);
+//        factory.setAdviceChain(RetryInterceptorBuilder.stateless()
+//                .maxAttempts(3) // 최대 재시도 횟수 설정
+//                .recoverer(new RejectAndDontRequeueRecoverer()) // 실패 시 메시지 처리 중단
+//                .build());
+//        return factory;
+//    }
+}
