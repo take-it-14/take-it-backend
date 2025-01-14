f474b4dbcbe741b8ad91272873687fdba7bd2c26
MinJeong LEE <alswjd6807@naver.com>
2025-01-15 01:26:27 +0900

    [Fix] : 바인딩 이름 통일 및 직렬화 설정 추가

--- payment-service/src/main/java/com/takeit/payment/infrastructure/config/RabbitMQConfig.java
+++ payment-service/src/main/java/com/takeit/payment/infrastructure/config/RabbitMQConfig.java
@@ -5,6 +5,8 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +18,7 @@ public class RabbitMQConfig {
     @Value("${message.exchange}")
     private String exchange;
 
-    @Value("${message.queue.order.cancel}")
+    @Value("${message.queues.order.cancel}")
     private String queueOrder;
 
     @Bean
@@ -27,5 +29,10 @@ public class RabbitMQConfig {
 
     @Bean
     public Binding orderBinding() { return BindingBuilder.bind(orderQueue()).to(paymentExchange()).with(queueOrder); }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
 
 }
