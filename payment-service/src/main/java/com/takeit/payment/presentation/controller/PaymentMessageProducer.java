1998deb6d8cb682c39db4344486dfe18f8eb656d
MinJeong LEE <alswjd6807@naver.com>
2025-01-15 01:28:06 +0900

    [Feat] : OrderComplete 로직 구현

---
+++ payment-service/src/main/java/com/takeit/payment/presentation/controller/PaymentMessageProducer.java
@@ -0,0 +1,31 @@
+package com.takeit.payment.presentation.controller;
+
+import java.util.UUID;
+
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.takeit.payment.application.dto.order.OrderCompleteDto;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PaymentMessageProducer {
+
+	private final RabbitTemplate rabbitTemplate;
+	private final TopicExchange paymentExchange;
+
+	@Value("${message.queues.order.complete}")
+	private String orderComplete;
+
+	private void sendMessage(String routingKey, Object message) {
+		rabbitTemplate.convertAndSend(paymentExchange.getName(), routingKey, message);
+	}
+
+	public void sendOrderCompleteRequest(UUID orderId){
+		sendMessage(orderComplete, OrderCompleteDto.from(orderId));
+	}
+}
