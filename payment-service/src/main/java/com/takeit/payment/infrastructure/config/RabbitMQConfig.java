1c9f083c4747af3021b6933110f9b989d1531463
ykchoi1203 <chldudrms94@naver.com>
2025-01-14 12:23:16 +0900

    [Feat]: payment rabbitmq config 및 결제 취소 시 보상 트랜잭션 추가

---
+++ payment-service/src/main/java/com/takeit/payment/infrastructure/config/RabbitMQConfig.java
@@ -0,0 +1,31 @@
+package com.takeit.payment.infrastructure.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+
+    @Value("${message.exchange}")
+    private String exchange;
+
+    @Value("${message.queue.order.cancel}")
+    private String queueOrder;
+
+    @Bean
+    public TopicExchange paymentExchange() { return new TopicExchange(exchange); }
+
+    @Bean
+    public Queue orderQueue() { return new Queue(queueOrder); }
+
+    @Bean
+    public Binding orderBinding() { return BindingBuilder.bind(orderQueue()).to(paymentExchange()).with(queueOrder); }
+
+}
