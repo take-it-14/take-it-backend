f9aac2a85f0f53f16332837f0477adaf36608c23
ykchoi1203 <chldudrms94@naver.com>
2025-01-14 12:40:51 +0900

    [Feat]: rabbitListener 실패 횟수 설정 추가

--- order-service/src/main/java/com/takeit/order/infrastructure/config/RabbitMQConfig.java
+++ order-service/src/main/java/com/takeit/order/infrastructure/config/RabbitMQConfig.java
@@ -5,6 +5,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -33,4 +37,15 @@ public class RabbitMQConfig {
 
     @Bean
     public Binding productBinding() { return BindingBuilder.bind(productQueue()).to(orderExchange()).with(queueProduct); }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+        SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAdviceChain(RetryInterceptorBuilder.stateless()
+                .maxAttempts(3) // 최대 재시도 횟수 설정
+                .recoverer(new RejectAndDontRequeueRecoverer()) // 실패 시 메시지 처리 중단
+                .build());
+        return factory;
+    }
 }
