232af66ed60909f323a952c2556af90abc446e9e
MinJeong LEE <alswjd6807@naver.com>
2025-01-16 20:04:28 +0900

    [Feat] : Jackson2JsonMessageConverter 사용한 직렬화 설정

--- product-service/src/main/java/com/takeit/product/infrastructure/config/RabbitMQConfig.java
+++ product-service/src/main/java/com/takeit/product/infrastructure/config/RabbitMQConfig.java
@@ -9,6 +9,8 @@ import org.springframework.amqp.rabbit.config.RetryInterceptorBuilder;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.retry.RejectAndDontRequeueRecoverer;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -50,13 +52,20 @@ public class RabbitMQConfig {
     }
 
     @Bean
-    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter jsonMessageConverter) {
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
         factory.setConnectionFactory(connectionFactory);
         factory.setAdviceChain(RetryInterceptorBuilder.stateless()
                 .maxAttempts(3) // 최대 재시도 횟수 설정
                 .recoverer(new RejectAndDontRequeueRecoverer()) // 실패 시 메시지 처리 중단
                 .build());
+        factory.setMessageConverter(jsonMessageConverter);
         return factory;
     }
+
 }
