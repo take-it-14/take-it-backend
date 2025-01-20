54951cd8ac38dfdcaa810a24f3e0301e7e00f531
rua0704 <rua0704@gmail.com>
2025-01-20 22:33:26 +0900

    setting - google smtp 환경 변수 설정 & Email 전송 service 구현

---
+++ favorite-service/src/main/java/com/takeit/favorite/application/service/EmailService.java
@@ -0,0 +1,32 @@
+package com.takeit.favorite.application.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+    private final JavaMailSender javaMailSender;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public void sendMail(String to, String subject, String text) {
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setTo(to);
+            message.setSubject(subject);
+            message.setText(text);
+            message.setFrom(from);
+            javaMailSender.send(message);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+
+    }
+}
