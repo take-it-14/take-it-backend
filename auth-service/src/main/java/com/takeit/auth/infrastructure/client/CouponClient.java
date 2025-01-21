cf760185de907f4533c5cbcda0804d10e8596e8e
ykchoi1203 <chldudrms94@naver.com>
2025-01-21 12:38:03 +0900

    [Fix]: @RequestParam 으로 변경

--- auth-service/src/main/java/com/takeit/auth/infrastructure/client/CouponClient.java
+++ auth-service/src/main/java/com/takeit/auth/infrastructure/client/CouponClient.java
@@ -4,9 +4,10 @@ import com.takeit.auth.application.service.CouponService;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "coupon-service")
 public interface CouponClient extends CouponService {
     @PostMapping("/feign/v1/coupons/signup")
-    boolean createSignupUserCoupon(@RequestBody Long userId);
+    boolean createSignupUserCoupon(@RequestParam Long userId);
 }
